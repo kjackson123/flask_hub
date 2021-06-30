e75bccd08ea216a35997f54f8f5bdee723acda11
kjackson <kimberly.jackson.2@aptiv.com>
2021-06-30 14:34:56 -0400

    proc added

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:hubgui
+web: gunicorn -b :$PORT app:hubgui
--- hubgui.py
+++ hubgui.py
@@ -110,4 +110,5 @@ def user(usr):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
